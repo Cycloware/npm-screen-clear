a1def2e9144dc1620091f8b6c27139fac1d3ca66
Kavan J Shaban <ks@cycloware.com>
2017-06-12 22:27:08 -0400

    Added `.editorconfig`

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function clear() {
-   process.stdout.write('\033[2J');
-   process.stdout.write('\033[0f');
+  process.stdout.write('\033[2J');
+  process.stdout.write('\033[0f');
 }
 module.exports = clear;
 
 if (require.main === module) {
-   clear();
+  clear();
 }
